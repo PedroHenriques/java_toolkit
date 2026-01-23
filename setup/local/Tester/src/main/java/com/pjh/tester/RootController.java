9936a0c030e5b5797f264f01f548b8fc28664028
Pedro Henriques <pedro@pedrojhenriques.com>
2026-01-23 16:49:23 +0000

    Add interaction with Logger service in the local dev env.

--- setup/local/Tester/src/main/java/com/pjh/tester/RootController.java
+++ setup/local/Tester/src/main/java/com/pjh/tester/RootController.java
@@ -3,10 +3,21 @@ package com.pjh.tester;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pjh.toolkit.core.logging.MinLogLevel;
+import com.pjh.toolkit.core.logging.ToolkitLogger;
+
 @RestController
 public class RootController {
+
+  private final ToolkitLogger logger;
+
+  public RootController(ToolkitLogger logger) {
+    this.logger = logger;
+  }
+
   @GetMapping("/")
   public String hello() {
+    logger.log(MinLogLevel.INFORMATION, null, "GET / hit");
     return "hello world";
   }
 }
---
+++ setup/local/Tester/src/main/java/com/pjh/tester/config/ToolkitConfig.java
@@ -0,0 +1,34 @@
+package com.pjh.tester.config;
+
+import java.util.Map;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.pjh.toolkit.core.logging.ToolkitLogger;
+import com.pjh.toolkit.core.types.LoggerInputs;
+import com.pjh.toolkit.core.utils.LoggerUtils;
+
+@Configuration
+public class ToolkitConfig {
+
+  @Bean
+  public ToolkitLogger toolkitLogger() {
+    LoggerInputs inputs = LoggerUtils.prepareInputs(
+      "my-app",
+      "my-activity-source",
+      "startup"
+    );
+
+    ToolkitLogger logger = new ToolkitLogger(inputs);
+
+    // optional: "startup" log / scope
+    try (AutoCloseable scope = logger.beginScope(
+        Map.of("project.name", "myapp", "deployment.environment", "dev")
+    )) {
+      logger.log(com.pjh.toolkit.core.logging.MinLogLevel.INFORMATION, null, "Logger initialized");
+    } catch (Exception ignored) {}
+
+    return logger;
+  }
+}
