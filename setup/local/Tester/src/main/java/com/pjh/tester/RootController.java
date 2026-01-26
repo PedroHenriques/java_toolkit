b7f6edef90ddbef190da64bc3aa18f7bcdcf4bdb
Pedro Henriques <pedro@pedrojhenriques.com>
2026-01-26 12:41:03 +0000

    Add feature flag to the Tester project.

--- setup/local/Tester/src/main/java/com/pjh/tester/RootController.java
+++ setup/local/Tester/src/main/java/com/pjh/tester/RootController.java
@@ -3,6 +3,8 @@ package com.pjh.tester;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.launchdarkly.sdk.server.interfaces.FlagValueChangeEvent;
+import com.pjh.toolkit.core.featureflags.FeatureFlags;
 import com.pjh.toolkit.core.logging.MinLogLevel;
 import com.pjh.toolkit.core.logging.ToolkitLogger;
 
@@ -10,14 +12,65 @@ import com.pjh.toolkit.core.logging.ToolkitLogger;
 public class RootController {
 
   private final ToolkitLogger logger;
+  private final FeatureFlags featureFlags;
 
-  public RootController(ToolkitLogger logger) {
+  public RootController(ToolkitLogger logger, FeatureFlags ff) {
     this.logger = logger;
+    this.featureFlags = ff;
   }
 
   @GetMapping("/")
   public String hello() {
     logger.log(MinLogLevel.INFORMATION, null, "GET / hit");
     return "hello world";
+  }
+
+  @GetMapping("/featureflag/subscribe")
+  public String ffSubscribe() {
+    logger.log(MinLogLevel.INFORMATION, null, "GET /featureflag/subscribe hit");
+
+    String ffKey = "ctt-net-toolkit-tester-consume-kafka-events";
+
+    this.featureFlags.subscribeToValueChanges(
+      ffKey,
+      (FlagValueChangeEvent ev) -> {
+        this.logger.log(
+          MinLogLevel.INFORMATION, null, "Received Feature Flag event: old value '" + ev.getOldValue().stringValue() + "' to new value '" + ev.getNewValue().stringValue() + "'"
+        );
+      }
+    );
+
+    var msg = "Subscribed to flag with key: " + ffKey;
+    this.logger.log(MinLogLevel.INFORMATION, null, msg);
+
+    return msg;
+  }
+
+  @GetMapping("/featureflag/value")
+  public String ffGetValue() {
+    logger.log(MinLogLevel.INFORMATION, null, "GET /featureflag/value hit");
+
+    String ffKey = "ctt-net-toolkit-tester-consume-kafka-events";
+
+    var curValue = this.featureFlags.getBoolFlagValue(ffKey, true);
+
+    var msg = "The current value of the flag with key: " + ffKey + " is " + curValue;
+    this.logger.log(MinLogLevel.INFORMATION, null, msg);
+
+    return msg;
+  }
+
+  @GetMapping("/featureflag/cached-value")
+  public String ffGetCachedValue() {
+    logger.log(MinLogLevel.INFORMATION, null, "GET /featureflag/cached-value hit");
+
+    String ffKey = "ctt-net-toolkit-tester-consume-kafka-events";
+
+    var cachedValue = FeatureFlags.getCachedBoolFlagValue(ffKey);
+
+    var msg = "The cached value of the flag with key: " + ffKey + " is " + cachedValue;
+    this.logger.log(MinLogLevel.INFORMATION, null, msg);
+
+    return msg;
   }
 }
--- setup/local/Tester/src/main/java/com/pjh/tester/config/ToolkitConfig.java
+++ setup/local/Tester/src/main/java/com/pjh/tester/config/ToolkitConfig.java
@@ -5,8 +5,12 @@ import java.util.Map;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.pjh.toolkit.core.featureflags.FeatureFlags;
 import com.pjh.toolkit.core.logging.ToolkitLogger;
+import com.pjh.toolkit.core.types.EnvNames;
+import com.pjh.toolkit.core.types.FeatureFlagsInputs;
 import com.pjh.toolkit.core.types.LoggerInputs;
+import com.pjh.toolkit.core.utils.FeatureFlagsUtils;
 import com.pjh.toolkit.core.utils.LoggerUtils;
 
 @Configuration
@@ -30,5 +34,18 @@ public class ToolkitConfig {
     } catch (Exception ignored) {}
 
     return logger;
+  }
+
+  @Bean
+  public FeatureFlags toolkitFeatureFlag(ToolkitLogger logger) {
+    FeatureFlagsInputs ffInputs = FeatureFlagsUtils.prepareInputs(
+      System.getenv("LD_ENV_SDK_KEY"),
+      System.getenv("LD_CONTEXT_API_KEY"),
+      System.getenv("LD_CONTEXT_NAME"),
+      EnvNames.dev,
+      logger
+    );
+
+    return new FeatureFlags(ffInputs);
   }
 }
